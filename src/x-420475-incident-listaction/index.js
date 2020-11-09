1ca38d40975a4880bdab088c01e7da7a73642cc1
holyanytskyy <43244627+holyanytskyy@users.noreply.github.com>
2020-11-09 18:49:22 +0200

    Update index.js

--- src/x-420475-incident-listaction/index.js
+++ src/x-420475-incident-listaction/index.js
@@ -1,133 +1,134 @@
-import {createCustomElement, actionTypes} from '@servicenow/ui-core';
-const {COMPONENT_BOOTSTRAPPED} = actionTypes;
+import { createCustomElement, actionTypes } from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
-import "@servicenow/now-template-card"
+import '@servicenow/now-template-card'
 import styles from './styles.scss';
-import {createHttpEffect} from '@servicenow/ui-effect-http';
+import { createHttpEffect } from '@servicenow/ui-effect-http';
 import '@servicenow/now-modal'
 import '@servicenow/now-label-value';
 
-
+const { COMPONENT_BOOTSTRAPPED } = actionTypes;
 const view = (state, { updateState }) => {
-	const{incidents= [],modalOpened={}} = state
-	console.log(modalOpened);
+    const { 
+            incidents = [],
+            modalOpened = {} 
+    } = state
 
 	return (
-		<div>
-			{incidents.map(incident => { const isModalOpened = modalOpened[incident.sys_id] ? true : false; console.log(isModalOpened); return <div>
-			<now-template-card-assist
-				tagline={{ "icon": "tree-view-long-outline", "label": "Incident" }}
-				actions={[
-					{
-						"id": "open-"+incident.sys_id,
-						"label": "Open Record"
-					},
-					{
-						"id": "delete-"+incident.sys_id,
-						"label": "Delete"
-					}
-				]}
-				heading={{ "label": incident.short_description }}
-				content={[
-					{
-						"label": "Number",
-						"value": {
-							"type": "string",
-							"value": incident.number
-						}
-					},
-					{
-						"label": "State",
-						"value": {
-							"type": "string",
-							"value": incident.state
-						}
-					},
-					{
-						"label": "Assigment Group",
-						"value": {
-							"type": "string",
-							"value": incident.assigment_group.display_value
-						}
-					},
-					{
-						"label": "Assigned To",
-						"value": {
-							"type": "string",
-							"value": incident.assigned_to.display_value
-						}
-					},
-				]}
-				contentItemMinWidth="300" footerContent={{ "label": "Updated", "value": incident.updated_on }}
-				configAria={{}}
-			>
-			</now-template-card-assist>
-				<now-modal
-				contentFullWidth ={true}
-				opened={isModalOpened}
-				manageOpened={true}
-				size="md"
-				//headerLabel="Record Information"
-			>
-				<now-label-value-stacked
-					items={[
-						{"label":"Incident Number",
-						 "value":{
-							 "type":"string",
-							 "label":incident.number,
-							 "href":"#","underlined":true
-							}
-						},
-
-						{"label":"State",
-						"value":{
-							"type":"string",
-							"value":incident.state}
-						},
-
-						{
-							"label":"Opened At",
-							"value":{
-								type:"string",
-								"label":incident.updated_on,
-							}
-						},
-						{"label":"Assignment group",
-						"value":[{
-							"type":"string",
-							"value":incident.assigment_group.display_value
-							}]
-						},
-						{"label":"Assigned to",
-						"value":{
-							"type":"string",
-							"value":incident.assigned_to.display_value
-							}
-						}
-					]}
-					align="horizontal-equal"
-					size="md"
-					delimiter=",">
-
-					</now-label-value-stacked>
-
-
-			</now-modal>
-
-		</div>
-		}
-	)}
-
-
-
-
+	    <div>
+            {
+                incidents.map(incident => { 
+                    const isModalOpened = modalOpened[incident.sys_id] ? true : false; 
+                    return <div>
+                        <now-template-card-assist 
+                            tagline = {{ 'icon': 'tree-view-long-outline', 'label': 'Incident' }}
+                            actions = {[
+                                {
+                                    'id': 'open-' + incident.sys_id,
+                                    'label': 'Open Record'
+                                },
+                                {
+                                    'id': 'delete-' + incident.sys_id,
+                                    'label': 'Delete'
+                                }
+                            ]}
+                            heading = {{ 'label': incident.short_description }}
+                            content = {[
+                                {
+                                    'label': 'Number',
+                                    'value': {
+                                        'type': 'string',
+                                        'value': incident.number
+                                    }
+                                },
+                                {
+                                    'label': 'State',
+                                    'value': {
+                                        'type': 'string',
+                                        'value': incident.state
+                                    }
+                                },
+                                {
+                                    'label': 'Assigment Group',
+                                    'value': {
+                                        'type': 'string',
+                                        'value': incident.assigment_group.display_value
+                                    }
+                                },
+                                {
+                                    'label': 'Assigned To',
+                                    'value': {
+                                        'type': 'string',
+                                        'value': incident.assigned_to.display_value
+                                    }
+                                },
+                            ]}
+                            contentItemMinWidth = '300' 
+                            footerContent = {{ 
+                                                'label': 'Updated', 
+                                                'value': incident.updated_on
+                                            }}
+                            configAria = {{}}
+                        >
+                        </now-template-card-assist>
+                        <now-modal
+                            contentFullWidth = { true }
+                            opened = { isModalOpened }
+                            manageOpened = { true }
+                            size = 'md'
+                            ><now-label-value-stacked
+                                items = {[
+                                    {
+                                        'label': 'Incident Number',
+                                        'value': {
+                                            'type': 'string',
+                                            'label': incident.number,
+                                            'href': '#',
+                                            'underlined': true
+                                        }
+                                    },
+                                    {
+                                        'label': 'State',
+                                        'value': {
+                                            'type': 'string',
+                                            'value': incident.state
+                                        }
+                                    },
+                                    {
+                                        'label': 'Opened At',
+                                        'value': {
+                                            'type': 'string',
+                                            'label': incident.updated_on
+                                        }
+                                    },
+                                    {
+                                        'label': 'Assignment group',
+                                        'value': [{
+                                            'type': 'string',
+                                            'value': incident.assigment_group.display_value
+                                        }]
+                                    },
+                                    {
+                                        'label': 'Assigned to',
+                                        'value': {
+                                            'type': 'string',
+                                            'value': incident.assigned_to.display_value
+                                        }
+                                    }
+                                ]}
+                                align = 'horizontal-equal'
+                                size = 'md'
+                                delimiter = ','
+                            ></now-label-value-stacked>
+                        </now-modal>
+                </div>
+                }
+	        )}
 		</div>
 	);
 };
 
-
 createCustomElement('x-420475-incident-listaction', {
-	renderer: {type: snabbdom},
+	renderer: { type: snabbdom },
 	view,
 	styles,
 
@@ -135,51 +136,48 @@ createCustomElement('x-420475-incident-listaction', {
 
 	actionHandlers: {
 		[COMPONENT_BOOTSTRAPPED] : (coeffects) => {
-			const {dispatch} = coeffects;
-			dispatch("FETCH_INCIDENT", {
+			const { dispatch } = coeffects;
+			dispatch('FETCH_INCIDENT', {
 				'sysparm_display_value': true
 			});
-
 		},
-
 		'NOW_DROPDOWN_PANEL#ITEM_CLICKED': (coeffects) => {
-			const{action,updateState,dispatch, state} = coeffects
+			const { action, updateState, dispatch, state} = coeffects
 			const id = action.payload.item.id
-			const res = id.split("-")
+			const res = id.split('-')
 			const sysId = res[1]
-			const operation = res[0]
-			if (operation=='delete') {
-				dispatch("DELETE_INCIDENT",{
+            const operation = res[0]
+            
+			if (operation == 'delete') {
+				dispatch('DELETE_INCIDENT', {
 					sys_id: sysId
 				})
 			} else if (operation == 'open') {
 				const { modalOpened = {} } = state;
-
 				modalOpened[sysId] = true;
 				updateState({modalOpened})
 			}
-
 		},
         'FETCH_INCIDENT': createHttpEffect('api/now/table/incident', {
 			method: 'GET',
 			headers: {},
-			queryParams:'sysparm_display_value',
+			queryParams: 'sysparm_display_value',
 			successActionType: 'INCIDENT_FETCH_SUCCESS'
 	   	}),
 	   	'DELETE_INCIDENT': createHttpEffect('api/now/table/incident/:sys_id', {
 			method: 'DELETE',
 			headers: {},
-			pathParams:'sys_id',
+			pathParams: 'sys_id',
 			successActionType: 'INCIDENT_DELETE_SUCCESS'
 		}),
         'INCIDENT_FETCH_SUCCESS': (coeffects) => {
-			const{action,updateState} = coeffects
-			const{result} = action.payload;
+			const { action, updateState } = coeffects
+			const { result } = action.payload;
 			let incidents = [];
 			result.forEach(incident => {
 				incidents.push({
 					short_description: incident.short_description,
-					number:incident.number,
+					number: incident.number,
 					state: incident.state,
 					assigment_group: incident.assignment_group,
 					assigned_to: incident.assigned_to,
@@ -187,24 +185,20 @@ createCustomElement('x-420475-incident-listaction', {
 					sys_id: incident.sys_id
 				})
 			});
-
 			updateState({incidents})
-
-
 		},
 		'INCIDENT_DELETE_SUCCESS': (coeffects) => {
-			const{action,updateState,state} = coeffects
+			const { action, updateState, state } = coeffects
 			const updUrl = action.meta.request.updatedUrl
-			const res = updUrl.split("/")
+			const res = updUrl.split('/')
 			const sysId = res.pop()
-			const {incidents} = state;
+			const { incidents } = state;
 			const filtered = incidents.filter(item => item.sys_id != sysId);
 			updateState({incidents: filtered})
 		},
 		'NOW_MODAL#OPENED_SET': (coeffects) => {
-			const{action, state, updateState} = coeffects;
+			const { action, state, updateState } = coeffects;
 			updateState({modalOpened: {}})
 		}
     }
 });
-
